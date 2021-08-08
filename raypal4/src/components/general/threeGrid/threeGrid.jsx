8e2e493c55b53d7d915f7f3105308081ad4c3064
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-08-08 17:24:41 +0800

    all 3 options

--- raypal4/src/components/general/threeGrid/threeGrid.jsx
+++ raypal4/src/components/general/threeGrid/threeGrid.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./threeGrid.css";
 import RT from "./RT.svg";
+import { Link } from "react-router-dom";
 
 class ThreeGrid extends Component {
   render() {
@@ -8,26 +9,49 @@ class ThreeGrid extends Component {
       <React.Fragment>
         <div className="grid-container">
           <div className="row">
-            <div className="col s12 m7 l7 grid grid-1">
-              <img className="page-logo" src={RT} alt="RT" />
-              <div className="content-continer">
-                <img
-                  className="grid-image"
-                  src={this.props.img1}
-                  alt="Coffee Boi"
-                />
-                <h3 className="grid-title">{this.props.title}</h3>
-                <h4 className="grid-subtitle">{this.props.sub1}</h4>
+            <Link to="/">
+              <div className="col s12 m7 l7 grid grid-1">
+                <img className="page-logo" src={RT} alt="RT" />
+                <div className="content-continer">
+                  <img
+                    className="grid-image"
+                    src={this.props.img1}
+                    alt="Coffee Boi"
+                  />
+                  <h4 className="grid-title">{this.props.title}</h4>
+                  <h4 className="grid-subtitle">{this.props.sub1}</h4>
+                </div>
               </div>
-            </div>
+            </Link>
+
             <div className="col s12 m5 l5">
               <div className="row">
-                <div className="col s12 m12 l12 grid grid-2">
-                  <h3 className="grid-title">{this.props.title2}</h3>
-                </div>
-                <div className="col s12 m12 l12 grid grid-3">
-                  <h3 className="grid-title">{this.props.title3}</h3>
-                </div>
+                <Link to="/">
+                  <div className="col s12 m12 l12 grid grid-2">
+                    <div className="content-continer">
+                      <img
+                        className="side-image"
+                        src={this.props.img2}
+                        alt="minions"
+                      />
+                      <h4 className="grid-title">{this.props.title2}</h4>
+                      <h5 className="grid-subtitle">{this.props.sub2}</h5>
+                    </div>
+                  </div>
+                </Link>
+                <Link to="/">
+                  <div className="col s12 m12 l12 grid grid-3">
+                    <div className="content-continer">
+                      <img
+                        className="side-image"
+                        src={this.props.img3}
+                        alt="musicblob"
+                      />
+                      <h4 className="grid-title">{this.props.title3}</h4>
+                      <h5 className="grid-subtitle">{this.props.sub3}</h5>
+                    </div>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
