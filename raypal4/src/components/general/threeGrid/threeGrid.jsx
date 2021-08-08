97c945c8a8a992321119da77010df711ac5c7b28
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-08-08 15:17:55 +0800

    head bob done

--- raypal4/src/components/general/threeGrid/threeGrid.jsx
+++ raypal4/src/components/general/threeGrid/threeGrid.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./threeGrid.css";
+import coffeeboy from "./coffeeboy.svg";
 
 class ThreeGrid extends Component {
   state = {};
@@ -9,12 +10,17 @@ class ThreeGrid extends Component {
         <div className="grid-container">
           <div className="row">
             <div className="col s12 m7 l7 grid grid-1">
+              <img className="grid-image" src={coffeeboy} alt="CoffeeBoy" />
               <h1 className="grid-title">{this.props.title}</h1>
             </div>
             <div className="col s12 m5 l5">
               <div className="row">
-                <div className="col s12 m12 l12 grid grid-2"></div>
-                <div className="col s12 m12 l12 grid grid-3"></div>
+                <div className="col s12 m12 l12 grid grid-2">
+                  <h1 className="grid-title">{this.props.title}</h1>
+                </div>
+                <div className="col s12 m12 l12 grid grid-3">
+                  <h1 className="grid-title">{this.props.title}</h1>
+                </div>
               </div>
             </div>
           </div>
