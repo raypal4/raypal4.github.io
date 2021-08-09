ae6d7d7ff599716da795134cf7a25db74f7a465f
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-08-09 13:08:25 +0800

    basic transition working

--- raypal4/src/app.js
+++ raypal4/src/app.js
@@ -1,13 +1,27 @@
-import React, { useState, Component } from "react";
+import React, {
+  useEffect,
+  useState,
+  Component,
+  useContext,
+  Switch,
+} from "react";
 import NavBar from "./components/navbar.jsx";
 import HomePage from "./pages/homepage.jsx";
 import About from "./pages/about.jsx";
 import InstaPiano from "./pages/instapiano.jsx";
-
-import { Route } from "react-router-dom";
-import instapiano from "./pages/instapiano.jsx";
+import { Route, useLocation } from "react-router-dom";
+import { __RouterContext } from "react-router";
+import { useTransition, animated } from "react-spring";
 
 const App = () => {
+  var { location } = useContext(__RouterContext);
+
+  const transitions = useTransition(location, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   const menuButtonClickMain = () => {
     const sideBar = document.querySelector(".side-bar");
     if (sideBar.classList.contains("active")) {
@@ -15,18 +29,29 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log(location);
+  });
+
   return (
-    <React.Fragment>
+    <>
       <NavBar />
       <main
         className="content-container"
         onClick={menuButtonClickMain.bind(this)}
       >
-        <Route path="/" exact component={HomePage}></Route>
-        <Route path="/about" component={About}></Route>
-        <Route path="/music" component={InstaPiano}></Route>
+        {transitions((props, item) => (
+          <animated.div style={props}>
+            <switch location={item}>
+              <Route path="/" exact component={HomePage}></Route>
+              <Route path="/about" exact component={About}></Route>
+              <Route path="/music" exact component={InstaPiano}></Route>
+            </switch>
+          </animated.div>
+        ))}
       </main>
-    </React.Fragment>
+    </>
   );
 };
 
