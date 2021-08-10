8dc254b523114e1dbeff734c5351ecf154fb7098
Raynold Tan <56285628+raypal4@users.noreply.github.com>
2021-08-10 19:02:38 +0800

    Update threeGrid.jsx

--- raypal4/src/components/general/threeGrid/threeGrid.jsx
+++ raypal4/src/components/general/threeGrid/threeGrid.jsx
@@ -79,87 +79,82 @@ class ThreeGrid extends Component {
 
     return (
       <React.Fragment>
-        {[<div className="test"></div>].map((div, key) => (
-          <LeftIn key={key}>{div}</LeftIn>
-        ))}
-
         <div className="grid-container">
           <div className="row">
-            {/* {[ */}
-            <Link to={this.props.link1}>
-              <div className="col s12 m7 l7 grid grid-1">
-                <img className="page-logo" src={RT} alt="RT" />
-                <div className="content-continer">
-                  <img
-                    className="grid-image"
-                    src={this.props.img1}
-                    alt="Coffee Boi"
-                  />
-                  <h4 className="grid-title">
-                    <div className="jeader">
-                      {this.props.title.split("").map((word, key) => (
-                        <SqueezeSpring key={key}>{word}</SqueezeSpring>
-                      ))}
-                    </div>
-                  </h4>
-                  <h4 className="grid-subtitle">{this.props.sub1}</h4>
+            {[
+              <Link to={this.props.link1}>
+                <div className="col s12 m7 l7 grid grid-1">
+                  <img className="page-logo" src={RT} alt="RT" />
+                  <div className="content-continer">
+                    <img
+                      className="grid-image"
+                      src={this.props.img1}
+                      alt="Coffee Boi"
+                    />
+                    <h4 className="grid-title">
+                      <div className="jeader">
+                        {this.props.title.split("").map((word, key) => (
+                          <SqueezeSpring key={key}>{word}</SqueezeSpring>
+                        ))}
+                      </div>
+                    </h4>
+                    <h4 className="grid-subtitle">{this.props.sub1}</h4>
+                  </div>
                 </div>
-              </div>
-            </Link>
-
-            {/* ].map((div, key) => (
+              </Link>,
+            ].map((div, key) => (
               <LeftIn key={key}>{div}</LeftIn>
-            ))} */}
+            ))}
+
             <div className="col s12 m5 l5">
               <div className="row">
-                {/* {[ */}
-                <Link to={this.props.link2}>
-                  <div className="col s12 m12 l12 grid grid-2">
-                    <div className="content-continer">
-                      <img
-                        className="side-image"
-                        src={this.props.img2}
-                        alt="minions"
-                      />
-                      <h4 className="grid-title">
-                        <div className="jeader">
-                          {this.props.title2.split("").map((word, key) => (
-                            <SqueezeSpring key={key}>{word}</SqueezeSpring>
-                          ))}
-                        </div>
-                      </h4>
-                      <h5 className="grid-subtitle">{this.props.sub2}</h5>
+                {[
+                  <Link to={this.props.link2}>
+                    <div className="col s12 m12 l12 grid grid-2">
+                      <div className="content-continer">
+                        <img
+                          className="side-image"
+                          src={this.props.img2}
+                          alt="minions"
+                        />
+                        <h4 className="grid-title">
+                          <div className="jeader">
+                            {this.props.title2.split("").map((word, key) => (
+                              <SqueezeSpring key={key}>{word}</SqueezeSpring>
+                            ))}
+                          </div>
+                        </h4>
+                        <h5 className="grid-subtitle">{this.props.sub2}</h5>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-
-                {/* ].map((div, key) => (
+                  </Link>,
+                ].map((div, key) => (
                   <RightIn key={key}>{div}</RightIn>
-                ))} */}
-                {/* {[ */}
-                <Link to={this.props.link3}>
-                  <div className="col s12 m12 l12 grid grid-3">
-                    <div className="content-continer">
-                      <img
-                        className="side-image"
-                        src={this.props.img3}
-                        alt="musicblob"
-                      />
-                      <h4 className="grid-title">
-                        <div className="jeader">
-                          {this.props.title3.split("").map((word, key) => (
-                            <SqueezeSpring key={key}>{word}</SqueezeSpring>
-                          ))}
-                        </div>
-                      </h4>
-                      <h5 className="grid-subtitle">{this.props.sub3}</h5>
-                    </div>
-                  </div>
-                </Link>
+                ))}
 
-                {/* ].map((div, key) => (
+                {[
+                  <Link to={this.props.link3}>
+                    <div className="col s12 m12 l12 grid grid-3">
+                      <div className="content-continer">
+                        <img
+                          className="side-image"
+                          src={this.props.img3}
+                          alt="musicblob"
+                        />
+                        <h4 className="grid-title">
+                          <div className="jeader">
+                            {this.props.title3.split("").map((word, key) => (
+                              <SqueezeSpring key={key}>{word}</SqueezeSpring>
+                            ))}
+                          </div>
+                        </h4>
+                        <h5 className="grid-subtitle">{this.props.sub3}</h5>
+                      </div>
+                    </div>
+                  </Link>,
+                ].map((div, key) => (
                   <BtmIn key={key}>{div}</BtmIn>
-                ))} */}
+                ))}
               </div>
             </div>
           </div>
